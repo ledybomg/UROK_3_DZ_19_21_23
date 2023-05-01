a4ac66816e773d07adcce3fe248d58c9b865c3ca
ledybomg <101559476+ledybomg@users.noreply.github.com>
2023-05-01 18:24:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
-﻿// Задача 21
+﻿// Задача 19
 
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// A (3,6,8); B (2,1,-7), -> 15.84
+// 14212 -> нет
 
-// A (7,-5, 0); B (1,-1,9) -> 11.53v
+// 12821 -> да
 
-Console.WriteLine("Введите координаты точки A (x1, y1, z1): ");
-        double x1 = double.Parse(Console.ReadLine());
-        double y1 = double.Parse(Console.ReadLine());
-        double z1 = double.Parse(Console.ReadLine());
+// 23432 -> да
 
-        Console.WriteLine("Введите координаты точки B (x2, y2, z2): ");
-        double x2 = double.Parse(Console.ReadLine());
-        double y2 = double.Parse(Console.ReadLine());
-        double z2 = double.Parse(Console.ReadLine());
 
-        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+     Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
 
-       
-     Console.WriteLine("Расстояние между точками A и B: {0:F2}", distance);
+        int firstDigit = number / 10000;
+        int secondDigit = number % 10000 / 1000;
+        int fourthDigit = number % 100 / 10;
+        int fifthDigit = number % 10;
+
+        if (firstDigit == fifthDigit && secondDigit == fourthDigit) {
+            Console.WriteLine("Да");
+        } else {
+            Console.WriteLine("Нет");
+        }
+        
